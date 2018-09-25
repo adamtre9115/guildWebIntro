074300c9fd75a96d5ce0ab3ba8f82fbbbb1a24e8
adamtre9115 <treadam9115@gmail.com>
2018-09-25 06:07:21 -0400

    update validation

--- restaurantSite/js/restaurant.js
+++ restaurantSite/js/restaurant.js
@@ -3,70 +3,83 @@ const Name = document.getElementById("name");
 const Email = document.getElementById("email");
 const Phone = document.getElementById("phone");
 const Error = document.getElementById("error");
+let nameValid;
+let emailValid;
+let phoneValid;
 
-Name.addEventListener(
-  "input",
-  function(e) {
-    //allow only letters in name input
-    let pattern = new RegExp("^[a-zA-Z ]*$");
+const checkName = () => {
+  Name.addEventListener(
+    "input",
+    function(e) {
+      //allow only letters in name input
+      let pattern = new RegExp("^[a-zA-Z ]*$");
+  
+      //name input text value
+      let nameInput = Name.value;
+  
+      //check for validity and pattern match
+      if (Name.validity.valid && nameInput.match(pattern)) {
+        console.log("Name input is valid");
+        Error.innerHTML = "";
+        nameValid = true;
+      } else {
+        Error.innerHTML =
+          "<span class='text-danger'>Please enter valid text</span>";
+          nameValid = false;
+      }
+    },
+  );
+  
+}
 
-    //name input text value
-    let nameInput = Name.value;
+const checkEmail = () => {
+  Email.addEventListener(
+    "input",
+    function(e) {
+      // regex pattern to validate email
+      let pattern = new RegExp(
+        "[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?"
+      );
+  
+      // gather email input
+      let emailInput = Email.value;
+  
+      if (Email.validity.valid && emailInput.match(pattern)) {
+        console.log("Email input is valid");
+        Error.innerHTML = "";
+        emailValid = true
+      } else {
+        Error.innerHTML =
+          "<span class='text-danger'>Please enter a valid email</span>";
+          emailValid = false;
+      }
+    },
+  );
+}
 
-    //check for validity and pattern match
-    if (Name.validity.valid && nameInput.match(pattern)) {
-      console.log("Name input is valid");
-      Error.innerHTML = "";
-    } else {
-      Error.innerHTML =
-        "<span class='text-danger'>Please enter valid text</span>";
-    }
-  },
-  false
-);
-
-Email.addEventListener(
-  "input",
-  function(e) {
-    // regex pattern to validate email
-    let pattern = new RegExp(
-      "[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?"
-    );
-
-    // gather email input
-    let emailInput = Email.value;
-
-    if (Email.validity.valid && emailInput.match(pattern)) {
-      console.log("Email input is valid");
-      Error.innerHTML = "";
-    } else {
-      Error.innerHTML =
-        "<span class='text-danger'>Please enter a valid email</span>";
-    }
-  },
-  false
-);
-
-Phone.addEventListener(
-  "input",
-  function(e) {
-    // regex to checck for valid phone
-    let pattern = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
-
-    // gather phone input value
-    let phoneInput = Phone.value;
-
-    // if valid phone and pattern match
-    if (Phone.validity.valid && phoneInput.match(pattern)) {
-      console.log("phone is valid");
-      Error.innerHTML = "";
-    } else {
-      Error.innerHTML =
-        "<span class='text-danger'>Please enter a valid phone number</span>";
-    }
-  },
-  false
-);
+const checkPhone = () => {
+  Phone.addEventListener(
+    "input",
+    function(e) {
+      // regex to checck for valid phone
+      let pattern = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
+  
+      // gather phone input value
+      let phoneInput = Phone.value;
+  
+      // if valid phone and pattern match
+      if (Phone.validity.valid && phoneInput.match(pattern)) {
+        console.log("phone is valid");
+        Error.innerHTML = "";
+        phoneValid = true;
+      } else {
+        Error.innerHTML =
+          "<span class='text-danger'>Please enter a valid phone number</span>";
+          phoneValid = false;
+      }
+    },
+  );
+}
 
 
 Form.addEventListener("submit", function(e) {
@@ -74,22 +87,18 @@ Form.addEventListener("submit", function(e) {
     e.preventDefault();
 
     //if all validations clear
-    if (isValid) {
+    if (nameValid && emailValid && phoneValid) {
     console.log("let's submit");
     Error.innerHTML =
       "<span class='text-success'>Everything looks good!</span>";
   } else {
+    Error.innerHTML =
+      "<span class='text-danger'>Something doesn't look right. Please check your info.</span>"
     console.log("can't send just yet");
   }
 });
 
-const isValid = () => {
-  if (Name.validity.valid && Email.validity.valid && Phone.validity.valid) {
-    console.log("let's submit");
-    Error.innerHTML =
-      "<span class='text-success'>Look's good to submit!</span>";
-      return true;
-  } else {
-    return false;
-  }
-}
+
+checkName()
+checkEmail()
+checkPhone()
